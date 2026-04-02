0e4c242b6ea698cb582e080456430799c770201d
ruper07 <ruper07@gmail.com>
2026-04-02 02:16:03 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
 app.listen(PORT, () => console.log(`Gastos app en puerto ${PORT}`));
